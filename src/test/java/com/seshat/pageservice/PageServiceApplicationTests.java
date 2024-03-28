62724e2e7c2c92c2c82142d05a57f95b537f6dde
Janith11 <janithpramuditha042@gmail.com>
2024-03-28 07:43:23 +0530

    git init

---
+++ src/test/java/com/seshat/pageservice/PageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seshat.pageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
